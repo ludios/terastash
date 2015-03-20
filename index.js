8439f48a465a0fab5292985bda8574488a6c18bc
Ivan Kozik <ivan@ludios.org>
2015-03-20 03:48:36 +0000

    Implement CLI with commander

--- index.js
+++ index.js
@@ -4,16 +4,41 @@ import assert from 'assert';
 /**
  * Add a file into the Cassandra database.
  */
-function addFile(pathname) {
+export function addFile(pathname) {
 	let content = fs.readFileSync(pathname);
 }
 
-//console.log(process.argv);
-if(0 && process.argv.length) {
-	let command = process.argv[2];
-	if(command == 'add') {
-		let pathname = process.argv[3];
-		assert(pathname, "Need a file to add");
-		addFile(pathname);
+/**
+ * Add files into the Cassandra database.
+ */
+export function addFiles(pathnames) {
+	for(let p of pathnames) {
+		addFile(p);
 	}
+}
+
+export function getStashInfo(stashPath) {
+	try {
+		return JSON.parse(fs.readFileSync(`${stashPath}/.terastash.json`));
+	} catch(e) {
+		if(e.code != 'ENOENT') {
+			throw e;
+		}
+	}
+	return null;
+}
+
+/**
+ * Initialize a new stash
+ */
+export function initStash(stashPath, name) {
+	assert(name, "Name must not be empty");
+	assert(typeof name == 'string', `Name must be string, got ${typeof name}`);
+
+	if(getStashInfo(stashPath)) {
+		throw new Error(`${stashPath} already contains a .terastash.json`);
+	}
+	fs.writeFileSync(
+		`${stashPath}/.terastash.json`,
+		JSON.stringify({name: name}, null, 2));
 }
