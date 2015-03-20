7a5d8ac1f824a44d21f2468dec3d2b8c2cdb5e55
Ivan Kozik <ivan@ludios.org>
2015-03-20 04:24:24 +0000

    Implement stash-base finding

--- index.js
+++ index.js
@@ -1,11 +1,38 @@
 import fs from 'fs';
 import assert from 'assert';
+import path from 'path';
+import { sync as findParentDir } from 'find-parent-dir';
+
+function getStashInfo(stashPath) {
+	try {
+		return JSON.parse(fs.readFileSync(`${stashPath}/.terastash.json`));
+	} catch(e) {
+		if(e.code != 'ENOENT') {
+			throw e;
+		}
+	}
+	return null;
+}
+
+/**
+ * For a given pathname, return which directory is the terastash
+ * base, or `null` if there is no terastash base.
+ */
+function findStashBase(pathname) {
+	return findParentDir(path.dirname(path.resolve(pathname)), ".terastash.json");
+}
 
 /**
  * Add a file into the Cassandra database.
  */
 export function addFile(pathname) {
-	let content = fs.readFileSync(pathname);
+	const content = fs.readFileSync(pathname);
+	const stashBase = findStashBase(pathname);
+	if(!stashBase) {
+		throw new Error(`File ${pathname} is not inside a stash: could not find a .terastash.json in any parent directories.`);
+	}
+	const dbPath = pathname.replace(stashBase, "");
+	//console.log({stashBase, dbPath});
 }
 
 /**
@@ -15,17 +42,6 @@ export function addFiles(pathnames) {
 	for(let p of pathnames) {
 		addFile(p);
 	}
-}
-
-export function getStashInfo(stashPath) {
-	try {
-		return JSON.parse(fs.readFileSync(`${stashPath}/.terastash.json`));
-	} catch(e) {
-		if(e.code != 'ENOENT') {
-			throw e;
-		}
-	}
-	return null;
 }
 
 /**
