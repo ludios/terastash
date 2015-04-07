35793fa076f42b633c3496b6cfbfb64598f506ff
Ivan Kozik <ivan@ludios.org>
2015-04-07 12:55:08 +0000

    Refactor command-line processor

--- command.js
+++ command.js
@@ -2,7 +2,8 @@
 
 const terastash = require('.');
 const ol = terastash.ol;
-let program = require('commander');
+const assert = require('assert');
+const program = require('commander');
 
 // Ugly hack to avoid getting Function
 function stringOrNull(o) {
@@ -16,54 +17,53 @@ program
 	.command('init <name>')
 	.description(ol(`initializes a stash in this directory and creates corresponding Cassandra
 		keyspace with name ${terastash.CASSANDRA_KEYSPACE_PREFIX}<name>.
-		Name cannot be changed later.`));
+		Name cannot be changed later.`))
+	.action(function(name) {
+		assert(typeof name == "string", name);
+		terastash.initStash(process.cwd(), name);
+	});
 
 program
 	.command('destroy <name>')
-	.description(`Destroys Cassandra keyspace ${terastash.CASSANDRA_KEYSPACE_PREFIX}<name>`);
+	.description(`Destroys Cassandra keyspace ${terastash.CASSANDRA_KEYSPACE_PREFIX}<name>`)
+	.action(function(name) {
+		assert(typeof name == "string", name);
+		terastash.destroyKeyspace(name);
+	});
 
 program
 	.command('add <file...>')
-	.description('adds file(s) to database');
+	.description('adds file(s) to database')
+	.action(function(files) {
+		terastash.addFiles(files);
+	});
 
 program
 	.command('rm <file...>')
-	.description('removes file(s) from database');
+	.description('removes file(s) from database')
+	.action(function(files) {
+		terastash.removeFiles(files);
+	});
 
 program
-	.command('ls [dir...]')
+	.command('ls [path...]')
 	.description('list directory in the database')
 	.option('-n, --name <name>', 'Ignore .terastash.json and use this stash name')
-	.action(function(cmd, options) {
+	.action(function(paths, options) {
 		//console.log({cmd, options}); process.exit();
 		const name = stringOrNull(options.name);
-		if(name != null && cmd[0] == null) {
-			console.error("When using -n/--name, a path is required");
+		if(name != null && !paths.length) {
+			console.error("When using -n/--name, a database path is required");
 			process.exit(1);
 		}
-		terastash.lsPath(name, cmd[0] || '.');
+		terastash.lsPath(name, paths[0] || '.');
 	});
 
 program
 	.command('list-keyspaces')
-	.description('list all terastash keyspaces in Cassandra');
+	.description('list all terastash keyspaces in Cassandra')
+	.action(function(cmd, options) {
+		terastash.listKeyspaces();
+	});
 
 program.parse(process.argv);
-
-switch(program.args[0]) {
-	case 'init':
-		terastash.initStash(process.cwd(), program.args[1]);
-		break;
-	case 'destroy':
-		terastash.destroyKeyspace(program.args[1]);
-		break;
-	case 'add':
-		terastash.addFiles(program.args.slice(1));
-		break;
-	case 'rm':
-		terastash.removeFiles(program.args.slice(1));
-		break;
-	case 'list-keyspaces':
-		terastash.listKeyspaces();
-		break;
-}
