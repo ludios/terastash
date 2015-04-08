8389ba7f1e09a2546f774886064c2f43fe315af1
Ivan Kozik <ivan@ludios.org>
2015-04-08 17:07:48 +0000

    Wrap help text for commander because it doesn't wrap for us

--- bin/ts.js
+++ bin/ts.js
@@ -1,7 +1,6 @@
 "use strict";
 
 const terastash = require('..');
-const ol = terastash.ol;
 const assert = require('assert');
 const program = require('commander');
 
@@ -10,14 +9,23 @@ function stringOrNull(o) {
 	return typeof o == "string" ? o : null;
 }
 
+/**
+ * Replace repeating tabs and add trailing newline.
+ *
+ * This is our dumb hack to work around commander's lack of proper wrapping.
+ */
+function d(s) {
+	return s.replace(/\t+/g, "\t") + "\n";
+}
+
 program
 	.version('0.0.1');
 
 program
 	.command('init <name>')
-	.description(ol(`Initializes a stash in this directory and creates corresponding Cassandra
-		keyspace with name ${terastash.CASSANDRA_KEYSPACE_PREFIX}<name>.
-		Name cannot be changed later.`))
+	.description(d(`
+		Initializes a stash in this directory and creates corresponding
+		Cassandra keyspace with name ${terastash.CASSANDRA_KEYSPACE_PREFIX}<name>. Name cannot be changed later.`))
 	.action(function(name) {
 		assert(typeof name == "string", name);
 		terastash.initStash(process.cwd(), name);
@@ -25,7 +33,8 @@ program
 
 program
 	.command('destroy <name>')
-	.description(`Destroys Cassandra keyspace ${terastash.CASSANDRA_KEYSPACE_PREFIX}<name>`)
+	.description(d(`
+		Destroys Cassandra keyspace ${terastash.CASSANDRA_KEYSPACE_PREFIX}<name>`))
 	.action(function(name) {
 		assert(typeof name == "string", name);
 		terastash.destroyKeyspace(name);
@@ -33,7 +42,8 @@ program
 
 program
 	.command('add <file...>')
-	.description('Adds file(s) to database')
+	.description(d(`
+		Adds file(s) to database`))
 	.action(function(files) {
 		// TODO: support -n
 		terastash.addFiles(files);
@@ -41,7 +51,9 @@ program
 
 program
 	.command('nuke <file...>')
-	.description(ol(`Removes file(s) from database and their corresponding chunks, if any.
+	.description(d(`
+		Removes file(s) from database and their corresponding chunks, if any.
+		Does not emit error or warning if specified files are not in the database.
 		Does not remove corresponding local checkout of the file.`))
 	.action(function(files) {
 		// TODO: support -n
@@ -50,14 +62,16 @@ program
 
 program
 	.command('help')
-	.description('Output usage information')
+	.description(d(`
+		Output usage information`))
 	.action(function() {
 		program.help();
 	});
 
 program
 	.command('ls [path...]')
-	.description('List directory in the database')
+	.description(d(`
+		List directory in the database`))
 	.option('-n, --name <name>', 'Ignore .terastash.json and use this stash name')
 	.action(function(paths, options) {
 		//console.log({cmd, options}); process.exit();
@@ -71,7 +85,8 @@ program
 
 program
 	.command('list-keyspaces')
-	.description('List all terastash keyspaces in Cassandra')
+	.description(d(`
+		List all terastash keyspaces in Cassandra`))
 	.action(function(cmd, options) {
 		terastash.listKeyspaces();
 	});
