e3f12d443e9c90667f9cbf079427c94c011adc0f
Ivan Kozik <ivan@ludios.org>
2015-07-06 07:02:50 +0000

    Promisify only the fs functions we need to reduce startup time by ~22ms

--- fs-promisified.js
+++ fs-promisified.js
@@ -1,6 +1,27 @@
 "use strong";
 "use strict";
 
+const fs = require('fs');
 const Promise = require('bluebird');
 
-module.exports = Promise.promisifyAll(require('fs'));
+// Promisify only the functions we need to avoid a 24+ ms require
+// penalty with promisifyAll (tested Intel 4970K)
+module.exports = {
+	chmodAsync: Promise.promisify(fs.chmod),
+	closeAsync: Promise.promisify(fs.close),
+	futimesAsync: Promise.promisify(fs.futimes),
+	openAsync: Promise.promisify(fs.open),
+	readFileAsync: Promise.promisify(fs.readFile),
+	renameAsync: Promise.promisify(fs.rename),
+	statAsync: Promise.promisify(fs.stat),
+	truncateAsync: Promise.promisify(fs.truncate),
+	unlinkAsync: Promise.promisify(fs.unlink),
+	writeFileAsync: Promise.promisify(fs.writeFile),
+
+	createReadStream: fs.createReadStream,
+	createWriteStream: fs.createWriteStream,
+
+	readFileSync: fs.readFileSync,
+	writeFileSync: fs.writeFileSync,
+	existsSync: fs.existsSync
+};
