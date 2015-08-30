6c96cf9b302383c0691fbd6066a92e5c4c1e16d8
Ivan Kozik <ivan@ludios.org>
2015-08-30 21:03:31 +0000

    Adjust benchmark argument

--- bench_hasher.js
+++ bench_hasher.js
@@ -4,6 +4,6 @@
 const hasher = require('./hasher');
 const utils = require('./utils');
 
-const crcWriter = new hasher.CRCWriter(Number(process.argv[2]) * 1024);
+const crcWriter = new hasher.CRCWriter(Number(process.argv[2]));
 utils.pipeWithErrors(process.stdin, crcWriter);
 utils.pipeWithErrors(crcWriter, process.stdout);
