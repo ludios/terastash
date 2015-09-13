f2ce1fc4e498afc9eea16596d57a15aee6c9e1b8
Ivan Kozik <ivan@ludios.org>
2015-09-13 19:57:58 +0000

    DROP TABLE and TYPE before destroying keyspace, to possibly fix an error on the next 'ts init' if .chunk already exists

--- index.js
+++ index.js
@@ -1632,12 +1632,20 @@ function assertName(name) {
 
 const destroyStash = Promise.coroutine(function* destroyStash$coro(stashName) {
 	assertName(stashName);
-	yield doWithClient(getNewClient(), function destroyStash$doWithClient(client) {
-		return runQuery(
+	yield doWithClient(getNewClient(), Promise.coroutine(function* destroyStash$doWithClient(client) {
+		yield runQuery(
+			client,
+			`DROP TABLE IF EXISTS "${KEYSPACE_PREFIX + stashName}".fs;`
+		);
+		yield runQuery(
+			client,
+			`DROP TYPE IF EXISTS "${KEYSPACE_PREFIX + stashName}".chunk;`
+		);
+		yield runQuery(
 			client,
 			`DROP KEYSPACE "${KEYSPACE_PREFIX + stashName}";`
 		);
-	});
+	}));
 	const config = yield getStashes();
 	utils.deleteKey(config.stashes, stashName);
 	yield utils.writeObjectToConfigFile("stashes.json", config);
