e975aceff1700544bb06f4ae0bbdf7a745b7539f
Ivan Kozik <ivan@ludios.org>
2016-04-27 03:10:05 +0000

    Work on HTTP server some more

--- src/http.js
+++ src/http.js
@@ -4,14 +4,43 @@ const A = require('ayy');
 const T = require('notmytype');
 const http = require('http');
 
-function handleRequest(request, response) {
-	response.end('Hello');
+class StashServer {
+	constructor(stashes) {
+		T(stashes, T.list(T.string));
+		this.stashes = stashes;
+	}
+
+	_handleRequest(req, res) {
+		res.setHeader("X-Frame-Options", "DENY");
+		res.setHeader("X-Content-Type-Options", "nosniff");
+		res.setHeader("X-XSS-Protection", "1; mode=block");
+		res.setHeader("X-UA-Compatible", "IE=edge");
+		if(req.url === '/') {
+			res.setHeader("Content-Type", "text/html; charset=utf-8");
+			for(const stash of this.stashes) {
+				res.write(`<li><a href="${stash}/">${stash}</a>`);
+			}
+		} else {
+			res.write('404');
+		}
+		res.end();
+	}
+
+	handleRequest(req, res) {
+		try {
+			return this._handleRequest(req, res);
+		} catch(err) {
+			console.error(err.stack);
+			res.end();
+		}
+	}
 }
 
 function listen(host, port, stashes) {
 	T(host, T.string, port, T.number, stashes, T.list(T.string));
-	var server = http.createServer(handleRequest);
-	server.listen(port, host);
+	const stashServer = new StashServer(stashes);
+	const httpServer = http.createServer(stashServer.handleRequest.bind(stashServer));
+	httpServer.listen(port, host);
 	console.log(`HTTP server listening on ${host}:${port}`);
 }
 
