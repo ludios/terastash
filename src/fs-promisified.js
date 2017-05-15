6d9fc57b58f82ff454468048b262386f478ff119
Ivan Kozik <ivan@ludios.org>
2017-05-15 23:28:05 +0000

    Alignment

--- src/fs-promisified.js
+++ src/fs-promisified.js
@@ -6,22 +6,22 @@ const Promise = require('bluebird');
 // Promisify only the functions we need to avoid a 24+ ms require
 // penalty with promisifyAll (tested Intel 4790K)
 module.exports = {
-	chmodAsync: Promise.promisify(fs.chmod),
-	closeAsync: Promise.promisify(fs.close),
-	futimesAsync: Promise.promisify(fs.futimes),
-	openAsync: Promise.promisify(fs.open),
-	readFileAsync: Promise.promisify(fs.readFile),
-	renameAsync: Promise.promisify(fs.rename),
-	statAsync: Promise.promisify(fs.stat),
-	truncateAsync: Promise.promisify(fs.truncate),
-	unlinkAsync: Promise.promisify(fs.unlink),
-	writeFileAsync: Promise.promisify(fs.writeFile),
+	chmodAsync:        Promise.promisify(fs.chmod),
+	closeAsync:        Promise.promisify(fs.close),
+	futimesAsync:      Promise.promisify(fs.futimes),
+	openAsync:         Promise.promisify(fs.open),
+	readFileAsync:     Promise.promisify(fs.readFile),
+	renameAsync:       Promise.promisify(fs.rename),
+	statAsync:         Promise.promisify(fs.stat),
+	truncateAsync:     Promise.promisify(fs.truncate),
+	unlinkAsync:       Promise.promisify(fs.unlink),
+	writeFileAsync:    Promise.promisify(fs.writeFile),
 
-	createReadStream: fs.createReadStream,
+	createReadStream:  fs.createReadStream,
 	createWriteStream: fs.createWriteStream,
 
-	readFileSync: fs.readFileSync,
-	writeFileSync: fs.writeFileSync,
-	existsSync: fs.existsSync,
-	readdirSync: fs.readdirSync
+	readFileSync:      fs.readFileSync,
+	writeFileSync:     fs.writeFileSync,
+	existsSync:        fs.existsSync,
+	readdirSync:       fs.readdirSync
 };
