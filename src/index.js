ludios/terastash

commit 392eb376323450ba7ea52672ead009d4a9fa2adc

Tweak variable name

src/date-version
@@ -1,1 +1,1 @@
-2018-03-29T10:51:17.155142289Z
+2018-03-29T12:32:16.202891451Z

src/index.js
@@ -1867,10 +1867,10 @@ async function updateGoogleTokens(tokensFilename, clientId, clientSecret) {
 
 	// Write the file atomically to avoid the occasional
 	// `SyntaxError: Unexpected end of JSON input` while the file is still being written.
-	const tempPath = `${path.dirname(path.dirname(tokensFilename))}/temp`;
+	const tempDirectory = `${path.dirname(path.dirname(tokensFilename))}/temp`;
 	await utils.mkdirpAsync(tempPath);
 	const content = JSON.stringify({credentials: {[clientId]: gdriver._oauth2Client.credentials}}, null, 2);
-	atomicWriteFileSync(tokensFilename, content, tempPath);
+	atomicWriteFileSync(tokensFilename, content, tempDirectory);
 }
 
 function assertName(name) {

commit 2b7c74a18c87c698b6433056ac352eaf4a94474f

Fix bug introduced in 392eb376323450ba7ea52672ead009d4a9fa2adc

src/date-version
@@ -1,1 +1,1 @@
-2018-03-29T15:42:05.660122489Z
+2018-03-29T15:43:38.009191939Z

src/index.js
@@ -1868,7 +1868,7 @@ async function updateGoogleTokens(tokensFilename, clientId, clientSecret) {
 	// Write the file atomically to avoid the occasional
 	// `SyntaxError: Unexpected end of JSON input` while the file is still being written.
 	const tempDirectory = `${path.dirname(path.dirname(tokensFilename))}/temp`;
-	await utils.mkdirpAsync(tempPath);
+	await utils.mkdirpAsync(tempDirectory);
 	const content = JSON.stringify({credentials: {[clientId]: gdriver._oauth2Client.credentials}}, null, 2);
 	atomicWriteFileSync(tokensFilename, content, tempDirectory);
 }